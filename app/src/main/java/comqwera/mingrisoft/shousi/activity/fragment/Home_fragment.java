1cb71c4b7b7dfb1ed8d02afa79116f6500ae8449
casualleftZ <1214064362@qq.com>
2018-12-10 22:43:24 +0800

    test

---
+++ app/src/main/java/comqwera/mingrisoft/shousi/activity/fragment/Home_fragment.java
@@ -0,0 +1,20 @@
+package comqwera.mingrisoft.shousi.activity.fragment;
+
+import android.view.View;
+import comqwera.mingrisoft.shousi.activity.activity.R;
+
+
+public class Home_fragment extends Basefragment
+{
+    @Override
+    public View initview() {
+        View view=View.inflate(mcontext, R.layout.activity_home, null);
+
+        return view;
+    }
+
+    protected void initData(){
+         super.initDate();
+        }
+}
+
---
+++ app/src/main/java/comqwera/mingrisoft/shousi/activity/fragment/Type_fragment.java
@@ -0,0 +1,20 @@
+package comqwera.mingrisoft.shousi.activity.fragment;
+
+import android.view.View;
+import android.widget.TextView;
+
+public class Type_fragment extends Basefragment {
+
+    private TextView textView;
+    @Override
+    public View initview() {
+        textView=new TextView(mcontext);
+
+        return textView;
+    }
+    public void initDate(){
+        super.initDate();
+        textView.setText("nhg ");
+    }
+}
+
